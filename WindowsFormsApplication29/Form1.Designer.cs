d2abfffb55806e13ab8e6da68b98b8fef52e33a0
Erfan Pourshahriar <94805816+ErfanPourshahriar@users.noreply.github.com>
2025-07-12 23:25:07 +0330

    first commit

---
+++ WindowsFormsApplication29/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsApplication29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.secLbl = new System.Windows.Forms.Label();
+            this.minLbl = new System.Windows.Forms.Label();
+            this.hourLbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.lblsd = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // secLbl
+            // 
+            this.secLbl.Font = new System.Drawing.Font("Eras Demi ITC", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secLbl.Location = new System.Drawing.Point(318, 51);
+            this.secLbl.Name = "secLbl";
+            this.secLbl.Size = new System.Drawing.Size(87, 57);
+            this.secLbl.TabIndex = 0;
+            this.secLbl.Text = "00";
+            this.secLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // minLbl
+            // 
+            this.minLbl.Font = new System.Drawing.Font("Eras Demi ITC", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minLbl.Location = new System.Drawing.Point(199, 51);
+            this.minLbl.Name = "minLbl";
+            this.minLbl.Size = new System.Drawing.Size(87, 57);
+            this.minLbl.TabIndex = 0;
+            this.minLbl.Text = "00";
+            this.minLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hourLbl
+            // 
+            this.hourLbl.Font = new System.Drawing.Font("Eras Demi ITC", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hourLbl.Location = new System.Drawing.Point(77, 51);
+            this.hourLbl.Name = "hourLbl";
+            this.hourLbl.Size = new System.Drawing.Size(87, 57);
+            this.hourLbl.TabIndex = 0;
+            this.hourLbl.Text = "00";
+            this.hourLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Eras Demi ITC", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(155, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 57);
+            this.label4.TabIndex = 0;
+            this.label4.Text = ":";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Eras Demi ITC", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(280, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 57);
+            this.label5.TabIndex = 0;
+            this.label5.Text = ":";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(326, 133);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(109, 43);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "START";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(193, 133);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(109, 43);
+            this.stopBtn.TabIndex = 1;
+            this.stopBtn.Text = "STOP";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(60, 133);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(109, 43);
+            this.resetBtn.TabIndex = 1;
+            this.resetBtn.Text = "RESET";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // lblsd
+            // 
+            this.lblsd.AutoSize = true;
+            this.lblsd.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblsd.Location = new System.Drawing.Point(356, 95);
+            this.lblsd.Name = "lblsd";
+            this.lblsd.Size = new System.Drawing.Size(49, 13);
+            this.lblsd.TabIndex = 2;
+            this.lblsd.Text = "Seconds";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(237, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Minutes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(120, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hours";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 188);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblsd);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hourLbl);
+            this.Controls.Add(this.minLbl);
+            this.Controls.Add(this.secLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label secLbl;
+        private System.Windows.Forms.Label minLbl;
+        private System.Windows.Forms.Label hourLbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Label lblsd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication29/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication29
+{
+    public partial class Form1 : Form
+    {
+        bool isBtnStopEnabled = false;
+        int min = 0;
+        int sec = 0;
+        int hour = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            if (sec == 60)
+            {
+                sec=0;
+                min++;
+                if (min == 60)
+                {
+                    min=0;
+                    hour++;
+                }
+            }
+
+            if (sec < 10)
+                secLbl.Text = "0" + sec.ToString();
+            else
+                secLbl.Text = sec.ToString();
+            if (min < 10)
+                minLbl.Text = "0" + min.ToString();
+            else
+                minLbl.Text = min.ToString();
+            if (hour < 10)
+                hourLbl.Text = "0" + hour.ToString();
+            else
+                hourLbl.Text = hour.ToString();
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            if (isBtnStopEnabled == false)
+            {
+                isBtnStopEnabled = true;
+                stopBtn.Text = "RESUME";
+                timer1.Enabled = false;
+            }
+            else
+            {
+                isBtnStopEnabled = false;
+                stopBtn.Text = "PAUSE";
+                timer1.Enabled = true;
+            }
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            min = 0;
+            sec = 0;
+            hour = 0;
+            secLbl.Text = "0" + sec.ToString();
+            minLbl.Text = "0" + min.ToString();
+            hourLbl.Text = "0" + hour.ToString();
+
+        }
+    }
+}
